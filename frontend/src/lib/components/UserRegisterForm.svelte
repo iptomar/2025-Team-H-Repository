2359795925e037b95930fcffc314d75f5b96f27d
Jorge <aluno24166@gmail.com>
2025-03-29 18:49:08 +0000

    feat: switch to shadcn components and create register route

---
+++ frontend/src/lib/components/UserRegisterForm.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/';
+	import { Input } from '$lib/components/ui/input/';
+	import { Label } from '$lib/components/ui/label/';
+	import { cn } from '$lib/utils.js';
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+
+	let isLoading = false;
+	async function onSubmit() {
+		isLoading = true;
+
+		setTimeout(() => {
+			isLoading = false;
+		}, 3000);
+	}
+</script>
+
+<div class={cn('grid gap-6', className)} {...$$restProps}>
+	<form on:submit|preventDefault={onSubmit}>
+		<div class="grid gap-2">
+			<div class="grid gap-1">
+				<Label class="sr-only" for="email">Email</Label>
+				<Input
+					id="email"
+					placeholder="nome@ipt.pt"
+					type="email"
+					autocapitalize="none"
+					autocomplete="email"
+					autocorrect="off"
+					disabled={isLoading}
+				/>
+			</div>
+			<Button type="submit" disabled={isLoading}>
+				Registar Email
+			</Button>
+		</div>
+	</form>
+</div>
---
+++ frontend/src/routes/register/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import UserRegisterForm from '$lib/components/UserRegisterForm.svelte';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<main>
+	<div
+		class="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
+	>
+		<Button
+			href="/login"
+			variant="ghost"
+			class="absolute right-4 top-4 md:right-8 md:top-8"
+		>
+			Login
+		</Button>
+		<div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+			<div
+				class="absolute inset-0 bg-cover"
+				style=" background-image: url(https://portal2.ipt.pt/img/generico/estt-ext.jpg);"
+			></div>
+		</div>
+		<div class="lg:p-8">
+			<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+				<div class="flex flex-col space-y-2 text-center">
+					<h1 class="text-2xl font-semibold tracking-tight">Criar uma conta</h1>
+					<p class="text-sm text-muted-foreground">Introduza o seu email para criar conta</p>
+				</div>
+				<UserRegisterForm />
+				<p class="px-8 text-center text-sm text-muted-foreground">
+					Ao continuar, aceita os nossos
+					<a href="/terms" class="underline underline-offset-4 hover:text-primary">
+						Termos de Serviço
+					</a>
+					e
+					<a href="/privacy" class="underline underline-offset-4 hover:text-primary">
+						Política de Privacidade
+					</a>
+					.
+				</p>
+			</div>
+		</div>
+	</div>
+</main>
