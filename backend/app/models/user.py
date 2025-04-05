62781d86ed0ba640502540f400a417c1c7714541
Rebolas <60193826+Rebolas@users.noreply.github.com>
2025-04-05 20:15:24 +0100

    Atributo comissão em users
    Criado o atributo opcional comissão na tabela users.
    -É necessário rever se fica assim ou se é criado uma tabela para isto.

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -8,15 +8,21 @@ class User(Base):
 
     email = Column(String(255), primary_key=True, index=True)
     hashed_password = Column(String(255), nullable=False)
-
+    comissao = Column(String(50), nullable=True) #A debater se fica assim ou se é criada uma tabela
 
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
-
+    #Se for mantido este atributo é necessário alterar os UIs também
+    comissao: Optional[str] = Field(
+        None,
+        max_length=50,
+        description="Nome da comissão/zona universitária (ex: 'Tomar')",
+        example="Tomar")
 
 class UserInDB(BaseModel):
     email: EmailStr
+    comissao: Optional[str] = None  # Opcional na resposta também
 
     class Config:
         orm_mode = True
