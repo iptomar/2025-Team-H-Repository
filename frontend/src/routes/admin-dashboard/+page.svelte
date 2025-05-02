0f163af445fa7491ca256fa6f11bacea1a5b5da9
Rodrigo Sampaio <rodrigosampaio7777@gmail.com>
2025-05-02 21:22:51 +0100

    Added pagination and search user by name feature

--- frontend/src/routes/admin-dashboard/+page.svelte
+++ frontend/src/routes/admin-dashboard/+page.svelte
@@ -1,18 +1,6 @@
 <script lang="ts">
     import AdminDashboard from "$lib/components/AdminDashboard.svelte";
 </script>
-
-
-<nav class="bg-black text-white shadow-md">
-    <div class="flex items-center justify-between p-4">
-        <h1 class="text-2xl font-bold">Admin Dashboard</h1>
-        <div class="flex space-x-4">
-            <a href="/admin-dashboard" class="hover:underline">Dashboard</a>
-            <a href="/admin-users" class="hover:underline">Users</a>
-            <a href="/admin-settings" class="hover:underline">Settings</a>
-        </div>
-    </div>
-</nav>
 <div>
     <AdminDashboard />
 </div>
